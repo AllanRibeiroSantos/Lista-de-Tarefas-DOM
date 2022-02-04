567ee17a74bf14537ed25157aa1b17930d6804a5
allanribeirosantos <allan_kaduh@hotmail.com>
2022-02-04 14:36:42 -0300

    Adicionando projeto (nao completo)

---
+++ adicionaTarefa.js
@@ -0,0 +1,17 @@
+const inputText = document.querySelector("#textForm");
+const enviarButton = document.querySelector("#botaoForm");
+const elementoPai = document.querySelector('.conteudo');
+
+enviarButton.onclick = function () {
+    const divisaoCriada = document.createElement('div');
+    divisaoCriada.classList.add('listaDeTarefas');
+    elementoPai.appendChild(divisaoCriada);
+
+    divisaoCriada.innerHTML = `
+    <p class="listaDeTarefas__tarefa">${inputText.value}</p>
+    <button class="listaDeTarefas__tarefa--botao botao"><span class="material-icons-outlined botaoDone">done</span></button>
+    <button class="listaDeTarefas__tarefa--botao botao"><span class="material-icons-outlined botaoClose">close</span></button>
+    `;
+
+    inputText.value = ""; // Após clicar no botão, o campo de form é esvaziado
+}
---
+++ botoes.js
@@ -0,0 +1,40 @@
+// BOTÃO EXCLUSÃO DE TAREFA
+elementoPai.addEventListener('click', function(event){
+
+    // Essas duas variáveis devem ser declaradas dentro deste bloco, ou o navegador irá dizer que ambos são NULL por não ter sido criadas ainda
+    // O .botaoClose afeta apenas no span, caso eu clique no button o efeito não funciona, talvez eu tenha que mudar o HTML para ele ser apenas um button... tenho que pensar
+    const elementoTarefa = document.querySelectorAll('.listaDeTarefas');
+    const botaoDeleta = document.querySelectorAll('.botaoClose');
+
+    for (let contador = 0; contador < botaoDeleta.length; contador++){
+        if (event.target == botaoDeleta[contador]) {
+            elementoTarefa[contador].remove();
+        }
+    }
+})
+
+// BOTÃO TAREFA CONCLUÍDA
+elementoPai.addEventListener('click', function(event){
+
+    const elementoTarefa = document.querySelectorAll('.listaDeTarefas__tarefa');
+    const botaoConcluido = document.querySelectorAll('.botaoDone');
+
+    for (let contador = 0; contador < botaoConcluido.length; contador++){
+        if (event.target == botaoConcluido[contador]) {
+            elementoTarefa[contador].classList.add('listaDeTarefas__tarefa--riscado');
+        }
+    }
+})
+
+// BOTÃO APAGAR LISTA
+
+// const apagarLista = document.querySelector('input[value="Apagar lista"]')
+
+// apagarLista.addEventListener('click', function(){
+//     const elementoTarefa = document.querySelector('.conteudo__tarefas');
+//     elementoTarefa.remove();
+
+//     const divisaoCriada = document.createElement('div');
+//     divisaoCriada.classList.add('listaDeTarefas');
+//     elementoPai.appendChild(divisaoCriada);
+// })
