3c679dcab4e6e1db7248978b8d67dc090d7d48cd
allanribeirosantos <allan_kaduh@hotmail.com>
2022-02-07 13:30:28 -0300

    Funcionalidade de mensagem de erro ao tentar enviar um texto vazio

--- adicionaTarefa.js
+++ adicionaTarefa.js
@@ -3,15 +3,34 @@ const enviarButton = document.querySelector("#botaoForm");
 const elementoPai = document.querySelector('.conteudo');
 
 enviarButton.onclick = function () {
-    const divisaoCriada = document.createElement('div');
-    divisaoCriada.classList.add('listaDeTarefas');
-    elementoPai.appendChild(divisaoCriada);
+    const divisaoCriada_tarefa = document.createElement('div');
+    const divisaoAviso = document.querySelector('.conteudo__formulario--aviso');
 
-    divisaoCriada.innerHTML = `
-    <p class="listaDeTarefas__tarefa">${inputText.value}</p>
-    <button class="listaDeTarefas__tarefa--botao botao botaoDoneBG"><span class="material-icons-outlined botaoDone">done</span></button>
-    <button class="listaDeTarefas__tarefa--botao botao botaoCloseBG"><span class="material-icons-outlined botaoCloseBG botaoClose">close</span></button>
-    `;
+    if (inputText.value) {
+        divisaoCriada_tarefa.classList.add('listaDeTarefas');
+        elementoPai.appendChild(divisaoCriada_tarefa);
 
-    inputText.value = ""; // Após clicar no botão, o campo de form é esvaziado
+        divisaoCriada_tarefa.innerHTML = `
+        <p class="listaDeTarefas__tarefa">${inputText.value}</p>
+        <button class="listaDeTarefas__tarefa--botao botao botaoDoneBG"><span class="material-icons-outlined botaoDone">done</span></button>
+        <button class="listaDeTarefas__tarefa--botao botao botaoCloseBG"><span class="material-icons-outlined botaoCloseBG botaoClose">close</span></button>
+        `;
+
+        inputText.value = ""; // Após clicar no botão, o campo de form é esvaziado
+
+        // Remove aviso de erro
+        if(divisaoAviso) {
+            divisaoAviso.remove();
+        }
+
+    // Adiciona mensagem de erro caso o campo esteja vazio
+    }else if (!divisaoAviso){
+        const elementoPai_section = document.querySelector('.conteudo__formulario--titulo');
+        const divisaoCriada_aviso = document.createElement('p');
+
+        divisaoCriada_aviso.classList.add('conteudo__formulario--aviso');
+        divisaoCriada_aviso.textContent = 'Tarefa não foi definida. Escreva algo no campo de texto.';
+
+        elementoPai_section.appendChild(divisaoCriada_aviso);
+    }
 }
